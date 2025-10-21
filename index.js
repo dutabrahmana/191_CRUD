30659af6a6f6c55a365f9f08db371b4499487bf3
dutabrahmana <dutalinggau25@gmail.com>
2025-10-21 15:03:21 +0700

    Menambahkan middleware untuk parsing JSON dan urlencoded

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-const express = require
+const express = require("express");
+let mysql = require("mysql2");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+
